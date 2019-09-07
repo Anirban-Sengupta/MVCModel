8558abee149adb64f5f0357ea5785e122718060c
Anirban Sengupta <sengupta.ani.80@gmail.com>
2019-09-06 17:26:33 -0700

    Used RequestParam Annotation

--- mvcmodel/src/main/java/mvcmodel/LoginController.java
+++ mvcmodel/src/main/java/mvcmodel/LoginController.java
@@ -1,18 +1,21 @@
 package mvcmodel;
 
-import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	@RequestMapping("/login")
-	public String display(HttpServletRequest req,Model m)  
+//	public String display(HttpServletRequest req,Model m)  
+//    {  
+//        String name=req.getParameter("name");  
+//        String pass=req.getParameter("pass"); 
+    public String display(@RequestParam("name")String name,@RequestParam("pass")String pass,Model m)  
     {  
-        String name=req.getParameter("name");  
-        String pass=req.getParameter("pass");  
         if(pass.equals("admin"))  
         {  
             String msg="Hello "+ name + ", you have successfully logged in";  
